6f5e2f5ce2da6783023fec0c39ea733a38be2ee5
José Ricardo Pérez <ararage@Joses-MacBook-Pro.local>
2019-04-13 09:20:22 -0500

    Added modal

--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -1,24 +1,66 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 import "./styles/BadgeDetails.css";
+import confLogo from "../images/platziconf-logo.svg";
+import Badge from "../components/Badge";
+import Modal from "../components/Modal";
 
-class BadgeDetails extends React.Component {
-  render() {
-    return (
-      <div>
-        <div className="BadgeDetails__hero">
-          <div className="container">
-            <div className="row">
-              <div className="col-6">
-                <img src={} alt="Logo de la conferencia"></img>
-              </div>
-              <div className="col-6" />
+function BadgeDetails(props) {
+  const badge = props.badge;
+  return (
+    <div>
+      <div className="BadgeDetails__hero">
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <img src={confLogo} alt="Logo de la conferencia" />
+            </div>
+            <div className="col-6 BadgeDetails__hero-attendant-name">
+              <h1>
+                {badge.firstName}
+                {badge.lastName}
+              </h1>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <Badge
+              firstName={badge.firstName}
+              lastName={badge.lastName}
+              email={badge.email}
+              twitter={badge.twitter}
+              jobTitle={badge.jobTitle}
+            />
+          </div>
+          <div className="col">
+            <h2>Actions</h2>
+            <div>
+              <div>
+                <Link
+                  className="btn btn-primary mb-4"
+                  to={`/badges/${badge.id}/edit`}
+                >
+                  Edit
+                </Link>
+              </div>
+
+              <div>
+                <button onClick={props.onOpenModal} className="btn btn-danger">
+                  Delete
+                </button>
+                <Modal isOpen={props.modalIsOpen} onClose={props.onCloseModal}>
+                  PEpe
+                </Modal>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default BadgeDetails;
